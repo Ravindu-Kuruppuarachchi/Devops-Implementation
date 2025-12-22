893cbfe495459d05e16d0619c8ab000ba6fb4800
Dhawala Rajakaruna <rajakarunarwmds.22@uom.lk>
2025-12-22 14:16:48 +0530

    final

--- main.py
+++ main.py
@@ -79,10 +79,6 @@ def get_stats(db: Session = Depends(get_db)):
         "sum": round(total_sum, 2),
         "count": count,
         "average": round(avg, 2).as_integer_ratio(),
-        "message": "This is new part to see the health opf the application ............",
-        "sum": round(total_sum, 2),
-        "count": count,
-        "average": round(avg, 2).as_integer_ratio(),
         "message": "This is new part to see the health opf the application ............"
     }
 
